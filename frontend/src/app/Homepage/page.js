0211774f0cd99dbbb57f4490b7734559184ff81f
walisaalam <walisaalam810@gmail.com>
2025-01-15 15:27:33 +0600

    commit

--- frontend/src/app/Homepage/page.js
+++ frontend/src/app/Homepage/page.js
@@ -8,7 +8,7 @@ const Homepage = () => {
         <div className='flex flex-col min-h-screen bg-background text-foreground'>
             <main className='flex flex-1 pt-16'>
                 <LeftSideBar/>
-                <div className="ml-160 flex-1 px-4 py-6 lg:max-w-2xl xl:max-w-3xl mx-auto">
+                <div className="ml-160 flex-1 px-4 py-6 lg:max-w-2xl xl:max-w-3xl h-full pb-16 mx-auto">
                     <HomeCard/>
                 </div>
             </main>
--- frontend/src/app/components/HomeCard.jsx
+++ frontend/src/app/components/HomeCard.jsx
@@ -1,27 +1,42 @@
+import { Separator } from '@/components/ui/separator';
+
 const HomeCard = () => {
     return (
-        <div className="relative flex flex-col top-20 bg-[#D8E0E4] items-center p-20 rounded-md shadow-md">
-            <div className='bg-white flex flex-col items-center space-y-16 mb-20 rounded-md p-4 shadow-md'>
+        <div className=" flex flex-col top-1/2 transform-translate-y-1/2 bg-[#D8E0E4] items-center p-16 rounded-md shadow-md m-8">
+            <div className='bg-white flex flex-col items-center space-y-16 mb-16 rounded-md p-4 shadow-md'>
                         
                 <div className="font-bold text-3xl">Total</div>
 
                 <div className="flex mx-20 space-x-40">
-                    <div className="font-bold text-xl">Staff: 49</div>
-                    <div className="font-bold text-xl">Students: 500</div>
-                </div>
-                        
+                    <div className='flex flex-col'>
+                        <div className="font-bold text-5xl" align="center">49</div>
+                        <Separator className="mt-4 w-28 bg-black"/>
+                        <div className='text-gray-400 text-md' align="center">No of Staff</div>
+                    </div>
+                    <div className='flex flex-col'>
+                        <div className="font-bold text-5xl" align="center"> 500</div>
+                        <Separator className="mt-4 w-28 bg-black"/>
+                        <div className='text-gray-400 text-md' align="center">No of Students</div>
+                    </div>
+                </div>           
             </div>
-            <div className='flex flex-col items-center space-y-2'>
-                <div className='bg-white flex flex-col items-center space-y-16 mb-4 rounded-md p-4 shadow-md'>
-                        <div className="font-bold text-3xl ">Current</div>
-        
-                        <div className="flex mx-20 space-x-40">
-                            <div className="font-bold text-xl">Staff: 49</div>
-                            <div className="font-bold text-xl">Students: 500</div>
-                        </div>     
-                    </div>        
-                    
-                </div>
+            <div className='bg-white flex flex-col items-center space-y-16 rounded-md p-4 shadow-md'>
+                        
+                <div className="font-bold text-3xl">Current</div>
+
+                <div className="flex mx-20 space-x-40">
+                    <div className='flex flex-col'>
+                        <div className="font-bold text-5xl" align="center">23</div>
+                        <Separator className="mt-4 w-28 bg-black"/>
+                        <div className='text-gray-400 text-md' align="center">No of Staff</div>
+                    </div>
+                    <div className='flex flex-col'>
+                        <div className="font-bold text-5xl" align="center"> 434</div>
+                        <Separator className="mt-4 w-28 bg-black"/>
+                        <div className='text-gray-400 text-md' align="center">No of Students</div>
+                    </div>
+                </div>           
+            </div>
         </div>
     )
 }
