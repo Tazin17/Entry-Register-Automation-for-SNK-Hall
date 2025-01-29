fee29d1ffbce85aeb50cebc3d6663f39a0d6ff5c
walisaalam <walisaalam810@gmail.com>
2025-01-29 09:32:54 +0600

    done

--- frontend/src/app/late-students/LateStudentsCard.jsx
+++ frontend/src/app/late-students/LateStudentsCard.jsx
@@ -2,104 +2,93 @@ import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/com
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import React from "react";
-// import { useRouter } from 'next/navigation';
 
 const LateStudentsCard = () => {
-    // const router=useRouter();
-    // const handleNavigation = (path,item)=>{
-    //     router.push(path);
-    //   };
+    
     const student_details = [
     {
         studentId: "2004004",
         studentName: "Rashfi Tabassum",
         roomNumber: "520",
+        lateEntry: ['01/01/2025 19:20:05', '02/01/2025 19:20:05', '05/01/2025 19:20:05'],
         contactNumber: "012345678",
     },
     {
         studentId: "2004012",
         studentName: "Fouzia Fariha",
         roomNumber: "423",
+        lateEntry: ['04/09/2024 19:20:05', '05/05/2024 19:20:05', '03/01/2025 19:20:05'],
         contactNumber: "012355678",
     },
     {
         studentId: "2004013",
         studentName: "Pritha Saha",
         roomNumber: "520",
+        lateEntry: [ '29/05/2024 19:20:05'],
         contactNumber: "013355678",
     },
     {
         studentId: "2004014",
         studentName: "Adiba Fairooz",
         roomNumber: "520",
+        lateEntry: ['09/09/2024 19:20:05', '29/05/2024 19:20:05'],
         contactNumber: "012355679",
     },
     {
         studentId: "2004015",
         studentName: "Walisa Alam",
         roomNumber: "424",
+        lateEntry: ['09/09/2024 19:20:05', '25/07/2024 19:20:05'],
         contactNumber: "012352679",
     },
     {
         studentId: "2004016",
         studentName: "Rashme Akther",
         roomNumber: "424",
+        lateEntry: ['02/09/2024 19:20:05'],
         contactNumber: "012355678",
     },
     {
         studentId: "2004017",
         studentName: "Nusrat Tazin",
         roomNumber: "424",
+        lateEntry: ['27/09/2024 19:20:05'],
         contactNumber: "012355679",
     },
     {
         studentId: "2004020",
         studentName: "Nabila Tabassum",
         roomNumber: "520",
+        lateEntry: ['12/03/2024 19:20:05'],
         contactNumber: "012355677",
     },
     {
         studentId: "2004023",
         studentName: "Arpita Mollik",
         roomNumber: "423",
+        lateEntry: ['18/07/2024 19:20:05'],
         contactNumber: "012355577",
     },
     {
         studentId: "2004027",
         studentName: "Shiti Chowdhury",
         roomNumber: "520",
-        contactNumber: "012355571",
-    },
-    {
-        studentId: "2004027",
-        studentName: "Shiti Chowdhury",
-        roomNumber: "520",
+        lateEntry: ['04/09/2024 19:20:05', '05/05/2024 19:20:05', '03/01/2025 19:20:05'],
         contactNumber: "012355571",
     },
     {
         studentId: "2004041",
         studentName: "Sidratul Muntaha",
         roomNumber: "208",
+        lateEntry: ['04/09/2024 19:20:05', '03/01/2025 19:20:05'],
         contactNumber: "012355579",
-    },
-    {
-        studentId: "2004044",
-        studentName: "Raisa Nazifa",
-        roomNumber: "218",
-        contactNumber: "021355579",
-    },
-    {
-        studentId: "2004045",
-        studentName: "Habiba Akhter",
-        roomNumber: "218",
-        contactNumber: "013255579",
-    },
+    }
     ]
     return ( 
         <div className="bg-red-2">
             <Card>
             <CardHeader>
-                <CardTitle className='text-2xl'>All Student Details</CardTitle>
+                <CardTitle className='text-2xl'>Late Student Details</CardTitle>
                 <CardDescription></CardDescription>
             </CardHeader>
             <CardContent>
@@ -108,8 +97,8 @@ const LateStudentsCard = () => {
                         <TableRow>
                             <TableHead>Student ID</TableHead>
                             <TableHead>Student Name</TableHead>
-                            <TableHead>Room Number</TableHead>
-                            <TableHead>Contact Number</TableHead>
+                            <TableHead>Late Frequency</TableHead>
+                            <TableHead>Latest Late Entry Time</TableHead>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
@@ -120,29 +109,37 @@ const LateStudentsCard = () => {
                                     <TableRow key={detail.studentId}>
                                         <TableCell>{detail.studentId}</TableCell>
                                         <TableCell>{detail.studentName}</TableCell>
-                                        <TableCell>{detail.roomNumber}</TableCell>
-                                        <TableCell>{detail.contactNumber}</TableCell>
+                                        <TableCell align='center'>{detail.lateEntry?.length}</TableCell>
+                                        <TableCell>{detail.lateEntry?.[detail.lateEntry?.length -1]}</TableCell>
                                     </TableRow>
                                 </DialogTrigger>
                                 <DialogContent>
                                     <DialogHeader>
-                                        <DialogTitle>Student Detail</DialogTitle>
-                                        <DialogDescription>All Info Regarding the Student</DialogDescription>
+                                        <DialogTitle>Late Student Details</DialogTitle>
+                                        <DialogDescription>All Late Info Regarding the Student</DialogDescription>
                                         <div >
                                             <div className="flex items-center">
-                                                <div className="font-semibold text-sm mr-2">Name :</div>
+                                                <div className="font-semibold text-sm w-[120px]">Name</div>
                                                 <div className="text-sm">{detail.studentName}</div>
                                             </div>
                                             <div className="flex items-center">
-                                                <div className="font-semibold text-sm mr-2">ID :</div>
+                                                <div className="font-semibold text-sm w-[120px]">ID</div>
                                                 <div className="text-sm">{detail.studentId}</div>
                                             </div>
                                             <div className="flex items-center">
-                                                <div className="font-semibold text-sm mr-2">Room Number :</div>
+                                                <div className="font-semibold text-sm w-[120px]">Late Frequency</div>
+                                                <div className="text-sm">{detail.lateEntry?.length}</div>
+                                            </div>
+                                            <div className="flex items-center">
+                                                <div className="font-semibold text-sm w-[120px]">Late Times</div>
+                                                <div className="text-sm">{ detail.lateEntry?.length > 0 ? detail.lateEntry.join(' , '): ''}</div>
+                                            </div>
+                                            <div className="flex items-center">
+                                                <div className="font-semibold text-sm w-[120px]">Room Number</div>
                                                 <div className="text-sm">{detail.roomNumber}</div>
                                             </div>
                                             <div className="flex items-center">
-                                                <div className="font-semibold text-sm mr-2">Contact :</div>
+                                                <div className="font-semibold text-sm w-[120px]">Contact</div>
                                                 <div className="text-sm">{detail.contactNumber}</div>
                                             </div>
                                         </div>
