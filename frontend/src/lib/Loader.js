858fce0b21ef798e4b2009ccb32b24097f5fff10
walisaalam <walisaalam810@gmail.com>
2025-02-18 22:35:15 +0600

    corrected avatar config

--- frontend/src/lib/Loader.js
+++ frontend/src/lib/Loader.js
@@ -38,7 +38,7 @@ export default function Loader() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.5, duration: 0.5 }}
       >
-        CUETbook
+        Entry Register Automation System
       </motion.div>
     </div>
   );
